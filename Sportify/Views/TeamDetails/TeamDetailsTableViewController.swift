a544ee3b1f9070d9a90d89a4a1b4450f515a322a
Amro <amrmu94@gmail.com>
2021-04-28 21:25:01 -0700

    adjusted team details UI

--- Sportify/Views/TeamDetails/TeamDetailsTableViewController.swift
+++ Sportify/Views/TeamDetails/TeamDetailsTableViewController.swift
@@ -54,7 +54,7 @@ class TeamDetailsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return 3
     }
     
     
